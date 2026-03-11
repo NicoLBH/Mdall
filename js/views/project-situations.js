d77470d6feeeae9f0638b22f4a7d39763c391c1e
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-11 12:09:48 +0100

    Update project-situations.js

--- js/views/project-situations.js
+++ js/views/project-situations.js
@@ -1225,7 +1225,6 @@ export function renderProjectSituations(root) {
   bindFilters();
   bindPagination();
   bindGlobal();
-  bindDetailsEvents();
   bindSidebarToggle();
   bindAssistantLauncher();
 
