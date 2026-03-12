1ea325f2b437861330242604c7a9e0990f762a8c
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-12 14:24:52 +0100

    Update project-situations-runbar.js

--- js/views/project-situations-runbar.js
+++ js/views/project-situations-runbar.js
@@ -11,10 +11,18 @@ export function renderProjectSituationsRunbar() {
     <div class="project-runbar">
       <div class="project-runbar__left">
         <div class="gh-split-btn" id="runSplitBtn">
+          <span data-component="icon">
+            <svg aria-hidden="true" focusable="false" class="octicon octicon-play" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm4.879-2.773 4.264 2.559a.25.25 0 0 1 0 .428l-4.264 2.559A.25.25 0 0 1 6 10.559V5.442a.25.25 0 0 1 .379-.215Z"></path></svg>
+          </span>
           <span class="gh-btn gh-btn--primary" id="runAnalysisBtnTop">Run analysis</span>
           <span class="gh-btn gh-btn--primary gh-btn--split" id="runMenuBtn" aria-label="Ouvrir le menu d’analyse">▼</span>
           <div class="gh-menu" id="runMenu">
-            <div class="gh-menu__item" data-action="run">Run analysis</div>
+            <div class="gh-menu__item" data-action="run">
+              <span data-component="icon">
+                <svg aria-hidden="true" focusable="false" class="octicon octicon-play" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm4.879-2.773 4.264 2.559a.25.25 0 0 1 0 .428l-4.264 2.559A.25.25 0 0 1 6 10.559V5.442a.25.25 0 0 1 .379-.215Z"></path></svg>
+              </span>
+              <span>Run analysis</span>
+            </div>
             <div class="gh-menu__item" data-action="reset">Reset</div>
           </div>
         </div>
