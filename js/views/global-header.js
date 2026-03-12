0736a8d0aea9a30b29da4887aa3a151584780e47
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-12 17:58:00 +0100

    Update global-header.js

--- js/views/global-header.js
+++ js/views/global-header.js
@@ -59,7 +59,9 @@ export function renderGlobalHeader() {
   host.innerHTML = `
     <header class="${model.headerClass}">
       <div class="gh-header__left">
-        <button id="menuBtn" class="icon-btn" type="button" aria-label="Ouvrir le menu">☰</button>
+        <button id="menuBtn" class="icon-btn" type="button" aria-label="Ouvrir le menu">
+          <svg aria-hidden="true" focusable="false" class="octicon octicon-three-bars" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"></path></svg>
+        </button>
 
         <a class="gh-brand" href="${model.href}">
           <img class="gh-brand__logo" src="logo.png" />
