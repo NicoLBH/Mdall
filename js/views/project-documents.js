81e940588e68f97a3245c4546314eaa93d73ba8e
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-13 18:43:15 +0100

    Update project-documents.js

--- js/views/project-documents.js
+++ js/views/project-documents.js
@@ -275,13 +275,14 @@ function renderUploadView() {
                     </span>
                   </label>
                 </div>
-
-                
               </section>
-              <div class="documents-commit-card__actions">
-                <button type="button" class="gh-btn gh-btn--validate" disabled>Valider</button>
-                <button type="button" class="gh-btn" id="documentsCancelBtn">Annuler</button>
-              </div>
+              
+              <section class="documents-commit-card">
+                <div class="documents-commit-card__actions">
+                  <button type="button" class="gh-btn gh-btn--validate" disabled>Valider</button>
+                  <button type="button" class="gh-btn" id="documentsCancelBtn">Annuler</button>
+                </div>
+              </section>
             </div>
           </div>
         </div>
