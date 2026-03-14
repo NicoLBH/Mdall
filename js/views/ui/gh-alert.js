fa1d0d652f48bedfd85aa86e7be3eb123064e680
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-14 12:35:37 +0100

    Update gh-alert.js

--- js/views/ui/gh-alert.js
+++ js/views/ui/gh-alert.js
@@ -1,4 +1,5 @@
 let ghAlertBound = false;
+import { svgIcon } from "../../ui/icons.js";
 
 function escapeHtml(value) {
   return String(value ?? "").replace(/[&<>"']/g, (char) => ({
@@ -32,9 +33,7 @@ export function renderGhAlert({
           data-gh-alert-close
           aria-label="Fermer"
         >
-          <svg viewBox="0 0 16 16" width="16" height="16" aria-hidden="true" fill="currentColor">
-            <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 1 1-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
-          </svg>
+          ${svgIcon("x")}
         </button>
       ` : ""}
 
