e51127db99595fcd17567632e074badee8d9698c
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-15 06:03:01 +0100

    Update overlay-chrome.js

--- js/views/ui/overlay-chrome.js
+++ js/views/ui/overlay-chrome.js
@@ -1,12 +1,4 @@
-function escapeHtml(value) {
-  return String(value ?? "").replace(/[&<>"']/g, (char) => ({
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#39;"
-  }[char]));
-}
+import { escapeHtml } from "../../utils/escape-html.js";
 
 export function renderOverlayChromeHead({
   eyebrow = "",
