23b68622d5f16d088bea047d1744becb026bcb0f
NicoLBH <nicolas.lebihan@socotec.com>
2026-04-02 17:18:38 +0200

    Update 20260401_043_enforce_valid_subject_event_types.sql

--- supabase/migrations/20260401_043_enforce_valid_subject_event_types.sql
+++ supabase/migrations/20260401_043_enforce_valid_subject_event_types.sql
@@ -1,17 +1,24 @@
-alter table subject_history
-add constraint subject_history_event_type_check
-check (
-  event_type in (
-    'subject_created',
-    'subject_enriched',
-    'subject_reopened',
-    'subject_marked_duplicate',
-    'subject_child_created',
-    'subject_link_created',
-    'status_changed',
-    'priority_changed',
-    'title_changed',
-    'parent_changed',
-    'situation_changed'
-  )
-);
+alter table public.subject_history
+  drop constraint if exists subject_history_event_type_check;
+
+alter table public.subject_history
+  add constraint subject_history_event_type_check
+  check (
+    event_type in (
+      'subject_created',
+      'subject_enriched',
+      'subject_reopened',
+      'subject_closed',
+      'subject_marked_duplicate',
+      'subject_replaced',
+      'subject_link_created',
+      'subject_child_created',
+      'subject_parent_assigned',
+      'subject_moved_to_situation',
+      'subject_moved_to_milestone',
+      'subject_assignee_changed',
+      'subject_title_updated',
+      'subject_description_updated',
+      'subject_status_changed'
+    )
+  );
