71bdaadb1f53e843ea1482133ddb8fa039ebb5aa
NicoLBH <nicolas.lebihan@socotec.com>
2026-04-03 18:50:49 +0200

    Update app.js

--- apps/web/js/app.js
+++ apps/web/js/app.js
@@ -1,4 +1,4 @@
-import { initRouter } from "./router.js";
+import { initRouter, rerenderRoute } from "./router.js";
 import { store } from "./store.js";
 import { mountAssistOverlay, bindGlobalAssistLauncher } from "./views/assist-overlay.js";
 import { renderGlobalShell } from "./views/global-shell.js";
@@ -6,8 +6,10 @@ import { runAnalysis, resetAnalysisUi } from "./services/analysis-runner.js";
 import { ensureProjectAutomationDefaults } from "./services/project-automation.js";
 import { bindGhActionButtons } from "./views/ui/gh-split-button.js";
 import { initializeDemoContext } from "./demo-context.js";
+import { PROJECT_IDENTITY_UPDATED_EVENT } from "./services/project-supabase-sync.js";
 
 let analysisEventsBound = false;
+let projectIdentityEventsBound = false;
 
 function bindAnalysisEvents() {
   if (analysisEventsBound) return;
@@ -23,12 +25,24 @@ function bindAnalysisEvents() {
   analysisEventsBound = true;
 }
 
+function bindProjectIdentityEvents() {
+  if (projectIdentityEventsBound) return;
+
+  window.addEventListener(PROJECT_IDENTITY_UPDATED_EVENT, () => {
+    renderGlobalShell();
+    rerenderRoute();
+  });
+
+  projectIdentityEventsBound = true;
+}
+
 function bootstrap() {
   console.log("RAPSOBOT V2 boot");
 
   initializeDemoContext();
 
   ensureProjectAutomationDefaults();
+  bindProjectIdentityEvents();
   bindAnalysisEvents();
   bindGhActionButtons();
 
