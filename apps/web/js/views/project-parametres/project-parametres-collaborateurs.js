b50528440119e7b8e51d0c9f075bd64526bcf4b7
NicoLBH <nicolas.lebihan@socotec.com>
2026-04-07 16:37:51 +0200

    Update project-parametres-collaborateurs.js

--- apps/web/js/views/project-parametres/project-parametres-collaborateurs.js
+++ apps/web/js/views/project-parametres/project-parametres-collaborateurs.js
@@ -1,11 +1,15 @@
-import { renderParametresSectionContent } from "./project-parametres-core.js";
+import {
+  renderCollaborateursParametresContent,
+  bindCollaborateursParametresSection
+} from "./project-parametres-core.js";
 
 export function getCollaborateursProjectParametresTab() {
   return {
     id: "parametres-collaborateurs",
     label: "Collaborateurs",
     iconName: "people",
-    false
-    renderContent: () => renderParametresSectionContent("parametres-collaborateurs")
+    isPrimary: false,
+    renderContent: () => renderCollaborateursParametresContent(),
+    bind: (root) => bindCollaborateursParametresSection(root)
   };
 }
