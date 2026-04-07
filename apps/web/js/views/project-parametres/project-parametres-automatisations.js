ba79992ada9d746643ce4489d2e7d77d0b8d5647
NicoLBH <nicolas.lebihan@socotec.com>
2026-04-07 16:37:31 +0200

    Update project-parametres-automatisations.js

--- apps/web/js/views/project-parametres/project-parametres-automatisations.js
+++ apps/web/js/views/project-parametres/project-parametres-automatisations.js
@@ -1,11 +1,15 @@
-import { renderParametresSectionContent } from "./project-parametres-core.js";
+import {
+  renderAutomatisationsParametresContent,
+  bindAutomatisationsParametresSection
+} from "./project-parametres-core.js";
 
 export function getAutomatisationsProjectParametresTab() {
   return {
     id: "parametres-automatisations",
     label: "Automatisations",
     iconName: "checklist",
-    false
-    renderContent: () => renderParametresSectionContent("parametres-automatisations")
+    isPrimary: false,
+    renderContent: () => renderAutomatisationsParametresContent(),
+    bind: (root) => bindAutomatisationsParametresSection(root)
   };
 }
