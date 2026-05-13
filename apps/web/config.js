c02dc17da3d2e2533eba95e8b655dfb4749ee7cd
NicoLBH <nicolas.lebihan@socotec.com>
2026-05-13 15:42:56 +0200

    Delete apps/web/config.js

--- apps/web/config.js
+++
@@ -1,5 +0,0 @@
-window.MDALL_CONFIG = window.MDALL_CONFIG || {};
-window.MDALL_CONFIG.supabaseUrl = window.MDALL_CONFIG.supabaseUrl || "https://nsdgfxnvwoieroyaxzxb.supabase.co";
-window.MDALL_CONFIG.supabaseAnonKey = window.MDALL_CONFIG.supabaseAnonKey || "";
-window.SUPABASE_URL = window.MDALL_CONFIG.supabaseUrl;
-window.SUPABASE_ANON_KEY = window.MDALL_CONFIG.supabaseAnonKey;
